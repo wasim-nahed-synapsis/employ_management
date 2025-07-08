277c766f0168dff561cb255910f3f19b0638ed70
Wasim Nahed <washimnahed0786@gmail.com>
2025-07-08 20:11:49 +0530

    remove unnessary code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { AuthService } from './auth.service';
-// import { UsersModule } from '../user/users.module';
-// import { JwtModule } from '@nestjs/jwt';
-// import { JwtStrategy } from './jwt.strategy';
-// import { AuthController } from './auth.controller';
-
-// @Module({
-//   imports: [
-//     UsersModule,
-//     JwtModule.register({
-//       secret: 'secret',
-//       signOptions: { expiresIn: '1d' },
-//     }),
-//   ],
-//   providers: [AuthService, JwtStrategy],
-//   controllers: [AuthController],
-// })
-// export class AuthModule {}
-
-
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersService } from '../user/users.service';
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,37 +1,3 @@
-// import { Injectable } from '@nestjs/common';
-// import { UsersService } from '../user/users.service';
-// import * as bcrypt from 'bcrypt';
-// import { JwtService } from '@nestjs/jwt';
-
-// @Injectable()
-// export class AuthService {
-//   constructor(
-//     private usersService: UsersService,
-//     private jwtService: JwtService
-//   ) {}
-
-//   async validateUser(email: string, pass: string): Promise<any> {
-//     const user = await this.usersService.findByEmail(email);
-    
-//     if (user && await bcrypt.compare(pass, user.password)) {
-//       return {
-//         id: user['_id'],
-//         email: user['email'],
-//       };
-//     }
-
-//     return null;
-//   }
-
-//   async login(user: any) {
-//     const payload = { email: user.email, sub: user.id }; // use user.id
-//     return {
-//       access_token: this.jwtService.sign(payload),
-//     };
-//   }
-// }
-
-
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../user/users.service';
 import * as bcrypt from 'bcrypt';

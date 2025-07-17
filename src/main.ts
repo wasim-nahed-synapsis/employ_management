bd663825ded53256a9bafd45b67af5a6ce06ba3e
Wasim Nahed <washimnahed0786@gmail.com>
2025-07-17 11:56:25 +0530

    deploye

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Allow CORS
+  app.enableCors({
+    origin: '*', // For production: replace * with frontend URL
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+
